81ff8b728d9d4e187defedc37937586aee3c726f
UMRzcz-831 <2488289735@qq.com>
2022-04-19 17:13:35 +0800

    feat: ipv4获取

--- src/middlewares/AccessLogger.ts
+++ src/middlewares/AccessLogger.ts
@@ -2,7 +2,7 @@ import { Context, Next } from 'koa'
 import { accessLogger } from './log4j'
 
 function AccessLogger(ctx: Context, next: Next) {
-  const logStr = `path:${ctx.path} | method:${ctx.method} | ua: ${ctx.headers['user-agent']}`
+  const logStr = `ip: ${ctx.request.ip}| path:${ctx.path} | method:${ctx.method} | ua: ${ctx.headers['user-agent']} `
   accessLogger.info(logStr)
   return next()
 }
